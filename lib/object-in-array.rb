5f0cab5b17547131d2cb5527e5f3f424b428f536
Lucas Clemente <luke.clemente@gmail.com>
2012-10-02 11:15:19 +0200

    implementation

---
+++ lib/object-in-array.rb
@@ -0,0 +1,7 @@
+require "object-in-array/version"
+
+class Object
+  def in?(array)
+    array.include? self
+  end
+end
---
+++ lib/object-in-array/version.rb
@@ -0,0 +1,7 @@
+module Object
+  module In
+    module Array
+      VERSION = "1.0"
+    end
+  end
+end
